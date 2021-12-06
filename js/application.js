594a16f60b0035425cf17c5e440bc1b893ccd4b6
Audun Hammer Hovda <hammerhovda@gmail.com>
2021-12-06 14:54:12 +0100

    Added web app funcitons

--- js/application.js
+++ js/application.js
@@ -1,3 +1,9 @@
+import { precacheAndRoute } from "workbox-precaching";
+
+// Use with precache injection
+precacheAndRoute(self.__WB_MANIFEST);
+
+
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
